92c723b5ae114f7eb834388b5240c1b462331afc
DominicDev <s0l0estudi4r@gmail.com>
2026-02-26 13:38:22 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'PETIT BISCUIT';
+export let nameSongs = [
+	"SUNSET LOVER", /*1*/
+	"ALONE", /*2*/
+	"NIGHT TROUBLE", /*3*/
+	"YOU", /*4*/
+	"JUNGLE", /*5*/
+	"FULL MOON", /*6*/
+	"ONCE AGAIN", /*7*/
+	"ICELAND", /*8*/
+	"FOLLOW ME", /*9*/
+	"PRESENCE", /*10*/
+	"BREAK UP", /*11*/
+	"WATERFALL ", /*12*/
+	"PROBLEMS (FEAT. LIDO)", /*13*/
+	"MEMORIES", /*14*/
+	"NIGHT", /*15*/
+	"MIDNIGHT SKY", /*16*/
+	"OCEAN", /*17*/
+	"PALMS", /*18*/
+	"RIGHT NOW", /*19*/
+	"GRAVITATION", /*20*/
+]
